9327824c89742009b745adb8bc32372be1e285d1
Musa Ibrahim <=>
2022-09-13 17:58:20 +0100

    Auto Commit

---
+++ johnny.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+filename = ""
+file = ""
+lines = ""
+
+try:
+    filename = sys.argv[1]
+except IndexError:
+    print("no argument passed")
+    sys.exit()
+
+try:
+    file = open(filename, "r")
+except FileNotFoundError:
+    print("File not found")
+    sys.exit()  
+print("working...")
+for line in file:
+    line = line.rstrip()    
+    if (line.find("if(")):
+        line = line.replace("if(", "if (")
+    if (line.find("for(")):
+        line = line.replace("for(", "for (")
+    if (line.find("while(")):
+        line = line.replace("while(", "while (")
+    lines = lines + line + "\n"
+print(lines)
+try:
+   file =  open(filename, "w")
+except FileNotFoundError:
+    print("System failure!")
+    exit()
+    
+file.write(lines)
+print("done")
